f364f2f7fc616486e720835ea2d411d6c9e7f5e3
Jan <50076082+janbsc4@users.noreply.github.com>
2020-06-11 12:11:39 +0200

    New sans-serif font

--- JavaScript/todo.js
+++
@@ -1,9 +0,0 @@
-console.log("This is your new ToDo app. You can see it by typing todoList and pressing enter. You can add items to it by using addItem(\"...\"). For example addItem(\"Go to gym\"). Delete the last item on the list by using todoList.pop(). Have fun!")
-
-var todoList = [
-	"Buy groceries"
-]
-
-function addItem(item) {
-	todoList.push(item);
-}
