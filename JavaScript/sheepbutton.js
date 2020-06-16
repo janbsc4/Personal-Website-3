2cdefd9e1d0d167a6f8e4510e2f4f6307a17d9d9
Jan <50076082+janbsc4@users.noreply.github.com>
2020-06-16 15:10:37 +0200

    Shorter JS for the sheepbutton functionality

--- JavaScript/sheepbutton.js
+++ JavaScript/sheepbutton.js
@@ -1,7 +1,5 @@
 const rollSheep = () => {
-	const button = document.querySelector("input[name='sheepButton']");
-	const sheep = document.querySelector("img.smiley")
-	sheep.classList.toggle("gosmiley")
+	document.querySelector("img.smiley").classList.toggle("gosmiley")
 }
 
 document.querySelector("input[name='sheepButton']").addEventListener("click", rollSheep);
