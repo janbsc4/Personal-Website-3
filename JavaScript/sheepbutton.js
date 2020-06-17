aee671774d54c0fd4afc6984c81c6fd434f5c757
Jan <50076082+janbsc4@users.noreply.github.com>
2020-06-17 11:23:08 +0200

    Code cleanup and redesign
    I rewrote the CSS to require significantly less styling HTML tags and cleaned up the HTML. Also I redesigned the header and footer to be more clearly visible and separate from the content.

--- JavaScript/sheepbutton.js
+++
@@ -1,5 +0,0 @@
-const rollSheep = () => {
-	document.querySelector("img.smiley").classList.toggle("gosmiley")
-}
-
-document.querySelector("input[name='sheepButton']").addEventListener("click", rollSheep);
